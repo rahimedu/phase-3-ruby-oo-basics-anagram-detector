ac3d59cec9d0f98778e0f0cbfb83a2b511325572
rahimedu <edwin.duche82@gmail.com>
2022-08-19 17:51:16 -0400

    initmessage

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(test)
+        letters = @word.chars
+        sortedLetters = letters.sort
+        finalWords = []
+
+        if test.class == String
+            sortedCompare=test.chars.sort
+            if sortedLetters == sortedCompare && sortedLetters.length == sortedCompare.length
+                finalWords << test
+            end
+        end
+
+        if test.class == Array
+            test.each do |i|
+                sortedCompare=i.chars.sort
+                if sortedLetters == sortedCompare && sortedLetters.length == sortedCompare.length
+                    finalWords << i
+                end
+            end
+        end 
+        finalWords
+    end
+end
